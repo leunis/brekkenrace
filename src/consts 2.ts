ce8093fcad3da7586f1d37eb8cfda44918ff57bf
macbook 2 <macbook2@macbooks-MacBook-Pro.local>
2024-07-02 09:08:14 +0200

    image component geintroduceerd

---
+++ src/consts 2.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Brekkenrace';
+export const SITE_DESCRIPTION = 'Welkom op de website van de Brekkenrace';
